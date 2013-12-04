97aebbc5b5ede5334d0640eb7f4016cd7c397a94
Luis Zandonadi Bissoli <luiszandonadi@gmail.com>
2013-12-04 13:59:06 -0200

    Update index.php

--- index.php
+++ index.php
@@ -2,68 +2,21 @@
 
 
 
-<div class="main <?php echo alx_layout_class(); ?>">
+<div class="content">
+	<?php if ( have_posts() ) : ?>
 
-	<div class="main-inner group">
+	<div class="post-list group">
 
-		
+		<?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
 
-		<section class="content">
+		<?php get_template_part('content'); ?>
 
-		
+		<?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
 
-			<?php get_template_part('inc/page-title'); ?>
+	</div><!--/.post-list-->
 
-			
+	<?php endif; ?>
 
-			<div class="pad group">
-
-				
-
-				<?php get_template_part('inc/featured'); ?>
-
-				
-
-				<?php if ( have_posts() ) : ?>
-
-				
-
-					<div class="post-list group">
-
-						<?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
-
-						<?php get_template_part('content'); ?>
-
-						<?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
-
-					</div><!--/.post-list-->
-
-				
-
-					<?php get_template_part('inc/pagination'); ?>
-
-					
-
-				<?php endif; ?>
-
-				
-
-			</div><!--/.pad-->
-
-			
-
-		</section><!--/.content-->
-
-		
-
-		<?php get_sidebar(); ?>
-
-		
-
-	</div><!--/.main-inner-->
-
-</div><!--/.main-->
-
-
+</div>
 
 <?php get_footer(); ?>
