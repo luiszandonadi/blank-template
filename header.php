1d6e56118c1d22bf711208dd0abcc8627f2746e2
Luis Zandonadi Bissoli <luiszandonadi@gmail.com>
2013-12-04 14:01:27 -0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+
+
+<head>
+
+	<meta charset="<?php bloginfo('charset'); ?>">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+	<title><?php wp_title(''); ?></title>
+
+
+
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+	
+
+	<?php wp_head(); ?>
+
+</head>
+
+
+
+<body>
+
+<div class="header"> </div>
