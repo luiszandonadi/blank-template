6192a5101b5bbfd786e2c202c8eb98e9b150b60b
Luis Zandonadi Bissoli <luiszandonadi@gmail.com>
2013-12-04 14:16:29 -0200

    Update header.php

--- header.php
+++ header.php
@@ -5,23 +5,12 @@
 
 
 <head>
-
 	<meta charset="<?php bloginfo('charset'); ?>">
-
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-
 	<title><?php wp_title(''); ?></title>
-
-
-
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-	
-
+	<link rel="stylesheet" type="text/css" href="./style.css" />
 	<?php wp_head(); ?>
-
 </head>
 
 
